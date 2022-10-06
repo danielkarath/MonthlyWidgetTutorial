04ddf0832cb37c286674190f48a3a4d173263a11
Daniel Karath <karath.dani@gmail.com>
2022-10-06 20:08:27 +0200

    Initial Commit

---
+++ MOnthly/MOnthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOnthlyApp.swift
+//  MOnthly
+//
+//  Created by Daniel Karath on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MOnthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
